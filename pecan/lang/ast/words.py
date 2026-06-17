c1fd8ab50459d37c5f1c521dbb819616579cf440
JoachimTreczoks <joachim.treczoks@uni-bonn.de>
2026-06-17 16:13:45 +0200

    Added type hinting to the AST index classes

--- pecan/lang/ast/words.py
+++ pecan/lang/ast/words.py
@@ -1,81 +1,85 @@
 #!/usr/bin/env python3.6
 # -*- coding=utf-8 -*-
 
-import spot
+from pecan.lang.ast.arith import IntConst
+from pecan.lang.ast.base import Predicate
+from pecan.lang.ast.bool import BoolConst
 
-from pecan.lang.ast import *
+from typing import TYPE_CHECKING
+if TYPE_CHECKING :
+    from pecan.lang.ast_transformer import AstTransformer
+    from pecan.lang.ast.base import Expression
 
 class Index(Predicate):
-    def __init__(self, var_name, index_expr):
+    def __init__(self, var_name : str, index_expr : Expression):
         super().__init__()
-        self.var_name = var_name
-        self.index_expr = index_expr
-        self.is_int = False
+        self.var_name : str = var_name
+        self.index_expr : Expression = index_expr
 
-    def transform(self, transformer):
+    def transform(self, transformer : AstTransformer) -> Index:
         return transformer.transform_Index(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '{}[{}]'.format(self.var_name, self.index_expr)
 
 class IndexRange(Predicate):
-    def __init__(self, var_name, start, end):
+    def __init__(self, var_name : str, start : Expression, end : Expression):
         super().__init__()
-        self.var_name = var_name
-        self.start = start
-        self.end = end
+        self.var_name : str = var_name
+        self.start : Expression = start
+        self.end : Expression = end
 
-    def transform(self, transformer):
+    def transform(self, transformer : AstTransformer) -> IndexRange:
         return transformer.transform_IndexRange(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return '{}[{}..{}]'.format(self.var_name, self.start, self.end)
 
 class EqualsCompareIndex(Predicate):
-    def __init__(self, is_equals, index_a, index_b):
+    def __init__(self, is_equals : bool, index_a : Index, index_b : IntConst | Index):
         super().__init__()
-        self.is_equals = is_equals
-        self.index_a = index_a
+        self.is_equals : bool = is_equals
+        self.index_a : Index = index_a
 
         if type(index_b) is IntConst:
             if index_b.val == 0:
-                self.index_b = BoolConst(False)
+                self.index_b : BoolConst = BoolConst(False)
             elif index_b.val == 1:
-                self.index_b = BoolConst(True)
+                self.index_b : BoolConst = BoolConst(True)
             else:
                 # TODO: Remove this restriction
-                raise Exception('Automatic words can only be binary (i.e., 0 or 1), {} is not allowed (in "{} = {}")'.format(b.val, a, b))
-        elif isinstance(index_b, Predicate):
-            self.index_b = index_b
+                raise Exception('Automatic words can only be binary (i.e., 0 or 1), {} is not allowed (in "{} = {}")'.format(index_b.val, index_a, index_b))
+        elif isinstance(index_b, Index):
+            self.index_b : Index = index_b
         else:
             raise Exception('Unexpected index expression on RHS: {} = {}'.format(index_a, index_b))
 
-    def transform(self, transformer):
+    def transform(self, transformer : AstTransformer) -> EqualsCompareIndex:
         return transformer.transform_EqualsCompareIndex(self)
 
-    def format(self, x):
+    def format(self, x : BoolConst | Index) -> IntConst | str:
         if isinstance(x, BoolConst):
             return IntConst(1 if x.bool_val else 0)
         else:
             return repr(x)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if self.is_equals:
             return '{} = {}'.format(self.index_a, self.format(self.index_b))
         else:
             return '{} ≠ {}'.format(self.index_a, self.format(self.index_b))
 
 class EqualsCompareRange(Predicate):
-    def __init__(self, is_equals, index_a, index_b):
+    def __init__(self, is_equals : bool, index_a : IndexRange, index_b : IndexRange):
         super().__init__()
-        self.is_equals = is_equals
-        self.index_a = index_a
-        self.index_b = index_b
+        self.is_equals : bool = is_equals
+        self.index_a : IndexRange = index_a
+        self.index_b : IndexRange = index_b
 
-    def transform(self, transformer):
+    def transform(self, transformer : AstTransformer) -> EqualsCompareRange:
         return transformer.transform_EqualsCompareRange(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if self.is_equals:
             return '{} = {}'.format(self.index_a, self.index_b)
         else:
