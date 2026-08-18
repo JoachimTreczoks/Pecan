8644adcac55026497b0e56ed0df149999c1b147a
JoachimTreczoks <joachim.treczoks@uni-bonn.de>
2026-08-18 21:45:33 +0200

    Bumped version number to 1.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Pecan",
-    version="1.1.1",
+    version="1.1.2",
     packages=find_packages(),
     scripts=['pecan.py'],
     install_requires=['lark', 'colorama', 'IPython', 'matplotlib']
