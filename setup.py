5211436876196035051c570b75e6f91f219726d3
JoachimTreczoks <joachim.treczoks@uni-bonn.de>
2026-09-02 17:38:06 +0200

    Made `setup.py` pull the version number directly from `versions.py`, to avoid any discrepancies

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from pecan.versions import pecan_version
 
 setup(
     name="Pecan",
-    version="1.1.2",
+    version=pecan_version,
     packages=find_packages(),
     scripts=['pecan.py'],
     install_requires=['lark', 'colorama', 'IPython', 'matplotlib']
